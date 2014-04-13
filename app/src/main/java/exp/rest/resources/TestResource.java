897a4ccf4718e227186aab9645b54b7a1de08e68
sha1n <sha1n@users.noreply.github.com>
2014-04-13 10:42:26 +0300

    Sample spring-boot + embedded Jetty + Jersey executable jar application

---
+++ app/src/main/java/exp/rest/resources/TestResource.java
@@ -0,0 +1,44 @@
+package exp.rest.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.MediaType;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * This is a sample resource.
+ *
+ * @author sha1n
+ * Date: 4/9/14
+ */
+@Path("/test")
+public class TestResource {
+
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    @GET
+    @Path("async")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void testAsync(@Suspended final AsyncResponse asyncResponse) {
+        executorService.execute(new Runnable() {
+            public void run() {
+                asyncResponse.resume(new Resp());
+            }
+        });
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Resp test() {
+        return new Resp();
+    }
+
+    public static class Resp {
+
+        public long timestamp = System.currentTimeMillis();
+    }
+}
